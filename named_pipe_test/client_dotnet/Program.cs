bfa56ae7926134a67c1e61988574c8b4f5eda098
Derek Wang <birdinforest@gmail.com>
2020-06-28 12:14:00 +1000

    Test instance of named pipes communication

---
+++ named_pipe_test/client_dotnet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Security.Principal;
+
+namespace client_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (NamedPipeClientStream pipeClient = new NamedPipeClientStream("localhost", "clouddeskpipeTest", PipeDirection.InOut, PipeOptions.None, TokenImpersonationLevel.None))
+                {
+                    pipeClient.Connect();
+                    using (StreamWriter sw = new StreamWriter(pipeClient))
+                    {
+                        sw.WriteLine("Hello world");
+                        sw.Flush();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ named_pipe_test/client_unity3d/Assets/Scenes/NamedPipeClient.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using UnityEngine;
+
+using System.Security.Principal;
+
+public class NamedPipeClient : MonoBehaviour
+{
+    // Init
+    public string content = "";
+    public int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI() {
+        content = GUILayout.TextArea(content, GUILayout.Width(200));
+        if(GUILayout.Button("SendData"))
+        {
+            SendData(content);
+        }
+    }
+
+    private static void SendData(string str)
+    {
+        try
+        {
+            using (NamedPipeClientStream pipeClient = new NamedPipeClientStream("localhost", "clouddeskpipeTest", PipeDirection.InOut, PipeOptions.None, TokenImpersonationLevel.None))
+            {
+                pipeClient.Connect();
+                using (StreamWriter sw = new StreamWriter(pipeClient))
+                {
+                    sw.WriteLine(str);
+                    sw.Flush();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.Message);
+        }
+    }
+}
