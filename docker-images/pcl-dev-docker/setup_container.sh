b47157676b47e1a44f0467b0c6ebb7a59b4b5048
Derek Wang <birdinforest@gmail.com>
2021-01-24 12:52:08 +1100

    Create minimum pcl dev image. Update shell scripts, add argument `-i [comp | min]` to setup compiled image or minimum image.

--- docker-images/pcl-dev-docker/setup_container.sh
+++ docker-images/pcl-dev-docker/setup_container.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #-------- X11 ---------------------
 
 # XSOCK=/tmp/.X11-unix
@@ -18,7 +20,7 @@
 # 	         -p 127.0.0.1:2222:22 \
 #            --user=$CONTAINER_USER \
 #            --volume=`pwd`/docker_dir:/home/$CONTAINER_USER/docker_dir \
-#            --volume=`pwd`/example_project:/home/$CONTAINER_USER/docker_dir/example_project \
+#            --volume=`pwd`/example_projects:/home/$CONTAINER_USER/docker_dir/example_projects \
 #            birdinforest/pcl-dev-docker:latest
 
 
@@ -26,13 +28,49 @@
 
 #-------- Non X11 ---------------------
 
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -i [comp | min]"
+   echo -e "comp: \tCreate container by image birdinforest/pcl-dev-docker."
+   echo -e "min: \tCreate container by image birdinforest/pcl-dev-docker-min."
+   exit 1 # Exit script after printing help
+}
+
+while getopts "i:" opt
+do
+   case "${opt}" in
+      i) parameterI="$OPTARG" ;;
+      ?) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$parameterI" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
 CONTAINER_USER=pcl
 
+if [ "$parameterI" == "min" ]; then
+    IMAGE=birdinforest/pcl-dev-docker-min:latest
+    CONTAINER_NAME=pcl-docker-min
+elif [ "$parameterI" == "comp" ]; then
+    IMAGE=birdinforest/pcl-dev-docker:latest
+    CONTAINER_NAME=pcl-docker-comp
+else
+    helpFunction
+fi
+
+echo "Setup container by image ${IMAGE}"
+
 docker run -it \
-        --name="pcl-docker" \
+      --name=${CONTAINER_NAME} \
 	    --cap-add sys_ptrace \
 	    -p 127.0.0.1:2222:22 \
         --user=$CONTAINER_USER \
         --volume=`pwd`/docker_dir:/home/$CONTAINER_USER/docker_dir \
-        --volume=`pwd`/example_project:/home/$CONTAINER_USER/docker_dir/example_project \
-        birdinforest/pcl-dev-docker:latest
+        --volume=`pwd`/example_projects:/home/$CONTAINER_USER/docker_dir/example_projects \
+        ${IMAGE}
