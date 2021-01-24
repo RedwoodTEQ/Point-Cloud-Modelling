0010c88ebc2762de47c04c2ea7709b46f95239cd
Derek Wang <birdinforest@gmail.com>
2021-01-24 13:02:06 +1100

    #8 Create minimum pcl dev image. Update shell scripts, add argument `-i [comp | min]` to setup/start compiled image or minimum image.

--- docker-images/pcl-dev-docker/build_image.sh
+++ docker-images/pcl-dev-docker/build_image.sh
@@ -1,1 +1,35 @@
-docker build ./docker -f docker/Dockerfile -t birdinforest/pcl-dev-docker
+#!/bin/bash
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -i [comp | min]"
+   echo -e "comp: \tBuild compiled image."
+   echo -e "min: \t\tBuild minimum image."
+   exit 1 # Exit script after printing help
+}
+
+while getopts "i:" opt
+do
+   case "${opt}" in
+      i) parameterI="$OPTARG" ;;
+      ?) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$parameterI" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+if [ "$parameterI" == "comp" ]; then
+   echo "Build compiled image.";
+   docker build ./docker/compiled-image -f docker/compiled-image/Dockerfile -t birdinforest/pcl-dev-docker
+elif [ "$parameterI" == "min" ]; then
+   echo "Build minimum image.";
+   docker build ./docker -f docker/minimum-image/Dockerfile -t birdinforest/pcl-dev-docker-min
+ else
+   helpFunction
+fi
--- docker-images/pcl-dev-docker/start_container.sh
+++ docker-images/pcl-dev-docker/start_container.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # ----------- X11 ---------------
 
 # XSOCK=/tmp/.X11-unix
@@ -18,6 +20,38 @@
 
 # ------------ Non X11 ----------
 
-docker start pcl-docker
-docker exec pcl-docker sudo service ssh start
-docker attach pcl-docker
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -i [comp | min]"
+   echo -e "comp: \tCreate container by image birdinforest/pcl-dev-docker."
+   echo -e "min: \tCreate container by image birdinforest/pcl-dev-docker-min."
+   exit 1 # Exit script after printing help
+}
+
+while getopts "i:" opt
+do
+   case "${opt}" in
+      i) parameterI="$OPTARG" ;;
+      ?) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$parameterI" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+if [ "$parameterI" == "min" ]; then
+    CONTAINER_NAME=pcl-docker-min
+elif [ "$parameterI" == "comp" ]; then
+    CONTAINER_NAME=pcl-docker-comp
+else
+    helpFunction
+fi
+
+docker start ${CONTAINER_NAME}
+docker exec ${CONTAINER_NAME} sudo service ssh start
+docker attach ${CONTAINER_NAME}
